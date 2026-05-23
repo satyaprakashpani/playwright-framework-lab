376fbf35b0e4dba292c6dcc0a6f676e9fe4e512f
Satyaprakash <satyaprakashpani@gmail.com>
2026-05-23 18:52:43 +0530

    remove: move grade calculator to dedicated if-else module

--- 03_module_variable_and_scope/07_IF_ELSE_Statemet/02_task2.js
+++
@@ -1,8 +0,0 @@
-let mark = 32;
-
-if (mark >= 90) console.log("Grade E");
-else if (mark >= 80) console.log("Grade A");
-else if (mark >= 70) console.log("Grade B")
-else if (mark >= 60) console.log("Grade C")
-else if (mark >= 50) console.log("Grade D")
-else console.log("Grade F --> Fail")
