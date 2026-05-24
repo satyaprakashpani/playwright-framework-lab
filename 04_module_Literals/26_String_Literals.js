1805556b0b1b8036b09157fe2668893af578c685
Satyaprakash <satyaprakashpani@gmail.com>
2026-05-24 20:11:12 +0530

    Add 26_String_Literals.js demonstrating single and double quote string creation

---
+++ 04_module_Literals/26_String_Literals.js
@@ -0,0 +1,20 @@
+// Single quotes
+let singleQuote = 'Hello World';
+console.log(typeof singleQuote);
+let withDoubleQuote = 'She said "hi"';
+console.log(typeof withDoubleQuote);
+
+// Double quotes
+let double = "Hello World";
+console.log(typeof double);
+let withSingle = "It's a test";
+console.log(typeof withSingle);
+
+
+let c = 'c';
+let c1 = 'cc';
+console.log(typeof c);
+console.log(typeof c1);
+
+
+// 'JavaScript prefers to use single code. '
