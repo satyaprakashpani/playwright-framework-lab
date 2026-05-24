ef17c061aa0919dec6ddfcbe7f63fe64e049d752
Satyaprakash <satyaprakashpani@gmail.com>
2026-05-24 20:11:12 +0530

    Add 23_null.js showing typeof behavior for null, undefined, and empty values

---
+++ 04_module_Literals/23_null.js
@@ -0,0 +1,11 @@
+let nullValue = null;
+let hello;
+
+console.log(typeof nullValue);
+console.log(typeof hello);
+
+let age = 0;
+
+let audi = "";
+console.log(typeof audi);
+console.log(typeof age);
