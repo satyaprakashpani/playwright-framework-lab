027e905044ce4a7f4928650834c2a98354b8bd24
Satyaprakash <satyaprakashpani@gmail.com>
2026-05-28 21:19:26 +0530

    Add 09_functions.js demonstrating JavaScript function basics and scope

---
+++ 03_module_variable_and_scope/09_functions.js
@@ -0,0 +1,21 @@
+console.log("Hi");
+console.log("Hi");
+console.log("Hi");
+
+function say() {
+    console.log("Hi from Function");
+}
+
+
+say();
+say();
+
+// 1. Define of function
+function greet() {
+    console.log("Hi, How are you?");
+}
+
+// 2. Calling of the function
+greet();
+greet();
+greet();
