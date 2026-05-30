a853994ca17724f73656b805ae9b58b2f360995d
Satyaprakash <satyaprakashpani@gmail.com>
2026-05-30 14:45:49 +0530

    Add 98_Type3_Fn_without_Param_Return_Type.js: function without parameter but with return statement

---
+++ 12_module_Functions/98_Type3_Fn_without_Param_Return_Type.js
@@ -0,0 +1,10 @@
+//No parameter but having return statement
+function goToRelativeHouse() {
+    console.log('Hi');
+    return "Uncle's House";
+}
+
+
+let returnedValue = goToRelativeHouse();//Hi
+console.log("---------------goToRelativeHouse() called----------------")
+console.log("returnedValue: " + returnedValue);
